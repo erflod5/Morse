3bc2efce6bc13f05a63dac5c39dd105405df221f
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-02-20 20:40:30 -0600

    First commit

---
+++ public/js/socket.js
@@ -0,0 +1,16 @@
+//var sock =new WebSocket("ws://localhost:5001");
+var sock = new WebSocket("ws://localhost:3000"); //replace this address with the one the node.js server prints out. keep port 3000
+var display = document.getElementById("display");
+sock.onopen = function(event) {
+  alert("Socket connected succesfully!!!");
+};
+
+sock.onmessage = function(event) {
+  console.log(event); //show received from server data in console of browser
+  display.innerHTML += event.data; //add incoming message from server to the log screen previous string + new string(message)
+  //traduccion(event.data);
+};
+
+function traduccion(cadenaMorse) {
+  display.innerHTML += cadenaMorse;
+}
