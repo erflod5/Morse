f2183f7d160514d6c296122a4978e4d5bde5712b
erflod5 <2883388711710@ingenieria.usac.edu.gt>
2020-02-20 21:27:36 -0600

    Salto de linea en texto recibido

--- public/js/socket.js
+++ public/js/socket.js
@@ -7,10 +7,9 @@ sock.onopen = function(event) {
 
 sock.onmessage = function(event) {
   console.log(event); //show received from server data in console of browser
-  display.innerHTML += event.data; //add incoming message from server to the log screen previous string + new string(message)
-  //traduccion(event.data);
+  display.innerHTML += event.data + "\n"; //add incoming message from server to the log screen previous string + new string(message)
 };
 
 function traduccion(cadenaMorse) {
-  display.innerHTML += cadenaMorse;
+  display.innerHTML += cadenaMorse + "\n";
 }
